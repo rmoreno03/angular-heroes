711dd1f5d7caa3743465bc791d76f3e788d4bb99
raul.moreno <raul.moreno@inercya.com>
2025-03-20 16:24:44 +0100

    Proyecto heroes terminado

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-layout-page',
-  standalone: false,
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: [
+  ],
+  standalone: false
 })
 export class LayoutPageComponent {
-  public sideBarItems = [
-    { label: 'Listado', icon: 'list', route: './list' },
-    { label: 'Añadir', icon: 'add', route: './new-hero' },
-    { label: 'Buscador', icon: 'search', route: './search' }
-  ]
+
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
+
 }
